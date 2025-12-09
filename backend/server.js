329ad59771e3e9c9a122ba1733801af768c41f9f
Asos75 <andraz.sosteric@gmail.com>
2025-12-09 11:19:46 +0100

    Revert "Multiple wire placment mode"
    This reverts commit 3d5d69e0f628d95a05bfabe9545a06dd47473031.

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
